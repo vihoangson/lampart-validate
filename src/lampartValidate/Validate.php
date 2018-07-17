3b83cfb8117dc876e01d7295f0f539999c3e825c
hoang_son <vihoangson@gmail.com>
2018-07-17 22:22:13 +0700

    commit

--- src/lampartValidate/Validate.php
+++ src/lampartValidate/Validate.php
@@ -3,6 +3,10 @@ namespace lampartValidate;
 
 class Validate{
 	public function __construct(){
-		echo 'hello world';
+		
+	}
+
+	public function validateInterger($value){
+		return is_integer($value);
 	}
 }
